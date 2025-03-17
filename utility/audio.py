b4e317322fecc4624c8a31b57967b100b5430923
AN SYU LI <yessir0621@gmail.com>
2025-03-17 14:43:51 +0800

    feat: faster to mp3

--- utility/audio.py
+++ utility/audio.py
@@ -2,24 +2,25 @@ import os
 import whisper
 from moviepy.editor import VideoFileClip
 import torch
-def convert_mp4_to_mp3(input_file, output_file=None):
+import subprocess
+def convert_mp4_to_mp3(input_file, output_file=None, bitrate="64k", sample_rate="32000"):
     if not output_file:
         output_file = os.path.splitext(input_file)[0] + ".mp3"
     
+
     try:
-        video = VideoFileClip(input_file)
-        audio = video.audio
-        audio.write_audiofile(output_file)
+        command = [
+            "ffmpeg", "-i", input_file, "-vn",  # Ignore video
+            "-b:a", bitrate, "-ar", sample_rate, "-ac", "1",  # Lower quality settings
+            "-map", "0:a", "-compression_level", "0",  # Faster audio extraction
+            "-preset", "ultrafast", "-y", output_file  # Fastest encoding settings
+        ]
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
         print(f"Conversion successful: {output_file}")
         return output_file
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error converting {input_file}: {e}")
         return None
-    finally:
-        if 'audio' in locals() and audio:
-            audio.close()
-        if 'video' in locals() and video:
-            video.close()
 
 def transcribe_audio(audio_path, model_size="medium"):  # Load large Whisper model
     if not os.path.exists(audio_path):
@@ -31,7 +32,7 @@ def transcribe_audio(audio_path, model_size="medium"):  # Load large Whisper mod
 
     model = whisper.load_model(model_size).to(device)
     print("Start transcribing...")
-    result = model.transcribe(audio_path)
+    result = model.transcribe(audio_path, fp16=False,  temperature=0)
     print("Transcription:")
     print(result.get("text", "No transcription available."))
     return result
